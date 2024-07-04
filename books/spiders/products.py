a27cb665945720b4087e3741c05e2135ba5dd031
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-07-04 11:44:37 +0200

    devide logic between different functions

--- books/spiders/products.py
+++ books/spiders/products.py
@@ -1,5 +1,6 @@
 import scrapy
 from scrapy.http import Response
+from typing import Generator, Optional
 
 
 class ProductsSpider(scrapy.Spider):
@@ -7,23 +8,55 @@ class ProductsSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
 
-    def parse(self, response: Response, **kwargs):
+    def parse(self, response: Response, **kwargs) -> Generator:
         for product in response.css("article.product_pod"):
             detail_page_url = product.css("h3 a::attr(href)").get()
             if detail_page_url:
-                yield response.follow(detail_page_url, callback=self.parse_detail)
+                yield response.follow(detail_page_url,
+                                      callback=self.parse_detail)
 
         next_page = response.css("li.next a::attr(href)").get()
         if next_page:
             yield response.follow(next_page, callback=self.parse)
 
-    def parse_detail(self, response: Response):
+    def parse_detail(self, response: Response) -> Generator:
         yield {
-            "title": response.css("div.product_main h1::text").get(),
-            "price": response.css("p.price_color::text").get().replace("£", ""),
-            "amount_in_stock": response.css("p.availability::text").re_first("\d+"),
-            "rating": response.css("p.star-rating::attr(class)").re_first("star-rating (\w+)"),
-            "category": response.css("ul.breadcrumb li:nth-child(3) a::text").get(),
-            "description": response.css("div#product_description ~ p::text").get(),
-            "upc": response.css("table.table tr:nth-child(1) td::text").get(),
+            "title": self.get_title(response),
+            "price": self.get_price(response),
+            "amount_in_stock": self.get_amount_in_stock(response),
+            "rating": self.get_rating(response),
+            "category": self.get_category(response),
+            "description": self.get_description(response),
+            "upc": self.get_upc(response),
         }
+
+    @staticmethod
+    def get_title(response: Response) -> Optional[str]:
+        return response.css("div.product_main h1::text").get()
+
+    @staticmethod
+    def get_price(response: Response) -> Optional[str]:
+        price = response.css("p.price_color::text").get()
+        return price.replace("£", "") if price else None
+
+    @staticmethod
+    def get_amount_in_stock(response: Response) -> Optional[str]:
+        return response.css("p.availability::text").re_first(r"\d+")
+
+    @staticmethod
+    def get_rating(response: Response) -> Optional[str]:
+        return response.css("p.star-rating::attr(class)").re_first(
+            r"star-rating (\w+)"
+        )
+
+    @staticmethod
+    def get_category(response: Response) -> Optional[str]:
+        return response.css("ul.breadcrumb li:nth-child(3) a::text").get()
+
+    @staticmethod
+    def get_description(response: Response) -> Optional[str]:
+        return response.css("div#product_description ~ p::text").get()
+
+    @staticmethod
+    def get_upc(response: Response) -> Optional[str]:
+        return response.css("table.table tr:nth-child(1) td::text").get()
